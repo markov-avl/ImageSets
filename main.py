4336f63c01c2be6a698cd931bff701b2f8a85462
markov-avl <70623307+markov-avl@users.noreply.github.com>
2021-12-12 21:03:30 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 from base64 import b64encode
 from aiohttp import ClientSession
 from json import dump
@@ -9,7 +10,7 @@ async def get_encoded_image(session: ClientSession) -> str:
         return b64encode(await response.content.read()).decode("utf-8")
 
 
-async def get_image_set(session: ClientSession) -> set:
+async def get_image_set(session: ClientSession) -> set[str]:
     return {await get_encoded_image(session) for _ in range(15)}  # количество фотографий в сете
 
 
@@ -22,7 +23,7 @@ async def perform_the_task(session: ClientSession, task_number: int) -> None:
     save_image_set(await get_image_set(session), task_number)
 
 
-async def main():
+async def main() -> None:
     async with ClientSession() as session:
         await asyncio.gather(*[perform_the_task(session, i) for i in range(100)])  # количество сетов
 
@@ -31,3 +32,4 @@ if __name__ == '__main__':
     loop = asyncio.new_event_loop()
     loop.run_until_complete(main())
     loop.close()
+    sys.exit()
